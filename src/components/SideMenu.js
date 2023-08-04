249323e4976d96a40efb4942a0408ac9c1c9fcb5
Vicky_Kang <93057655+KK6355@users.noreply.github.com>
2023-08-04 16:58:45 +1200

    created dynamic sidebar -menu with API endpoint

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,38 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  AppstoreOutlined,
+  ApartmentOutlined,
+  UserSwitchOutlined,
+  NotificationOutlined,
+  EditOutlined,
+  CarryOutOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
+import { Layout, Menu } from "antd";
+import "../views/sandbox/index.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const { Sider } = Layout;
 export default function SideMenu() {
   const [collapsed, setCollapsed] = useState(false);
 
+  const navigate = useNavigate();
+  const toPage = (e) => {
+    console.log(e.key);
+    navigate(e.key);
+  };
+  useEffect(() => {
+    axios.get("http://localhost:8000/rights?_embed=children").then((res) => {
+      console.log(res.data);
+      setMenu(res.data);
+    });
+  }, []);
+  const [menu, setMenu] = useState([]);
+
+  const items = [];
+  const iconList = {
+    "/home": <AppstoreOutlined />,
+    "/user-manage": <UserSwitchOutlined />,
+    "/right-manage": <ApartmentOutlined />,
+    "/news-manage": <NotificationOutlined />,
+    "/audit-manage": <EditOutlined />,
+    "/publish-manage": <CarryOutOutlined />,
+  };
+  menu.map((item) =>
+    item.children.length === 0
+      ? items.push({
+          key: item.key,
+          label: item.title,
+          icon: iconList[item.key],
+        })
+      : items.push({
+          key: item.key,
+          label: item.title,
+          icon: iconList[item.key],
+          children: item.children.map((data) => {
+            if (data.pagepermisson === 1) {
+              return { key: data.key, label: data.title };
+            } else {
+              return "";
+            }
+          }),
+        })
+  );
+
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
-      <div className="demo-logo-vertical" />
+      <div className="logo">Global News System</div>
       <Menu
-        theme="dark"
+        onClick={toPage}
+        defaultSelectedKeys={["/home"]}
         mode="inline"
-        defaultSelectedKeys={["1"]}
-        items={[
-          {
-            key: "1",
-            icon: <UserOutlined />,
-            label: "nav 1",
-          },
-          {
-            key: "2",
-            icon: <VideoCameraOutlined />,
-            label: "nav 2",
-          },
-          {
-            key: "3",
-            icon: <UploadOutlined />,
-            label: "nav 3",
-          },
-        ]}
+        items={items}
+        theme="dark"
       />
     </Sider>
   );
--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,5 +1,40 @@
+import { Button } from "antd";
 import React from "react";
+import axios from "axios";
 
 export default function Home() {
-  return <div>Home</div>;
+  const ajax = () => {
+    // axios.get("http://localhost:8000/posts").then((res) => {
+    //   console.log(res.data);
+    // });
+    // axios.post("http://localhost:8000/posts", {
+    //   title: "44444",
+    //   author: "xiaoming",
+    // });
+    // axios.put("http://localhost:8000/posts/1", {
+    //   title: "111-update",
+    //   author: "xiaoming",
+    // });
+    // axios.patch("http://localhost:8000/posts/1", {
+    //   title: "111-patch",
+    // });
+    // axios.delete("http://localhost:8000/posts/4");
+    // axios.get("http://localhost:8000/posts?_embed=comments").then((res) => {
+    //   console.log(res.data);
+    // });
+    // axios
+    //   .get(
+    //     "http://localhost:8000/commentsd:BaiduNetdiskDownload千锋2022版React全家桶教程_react零基础入门到项目实战完整版（资料）项目部分代码资料material\3-��Ŀʵս\014-���SideMenudb.json?_expend=post"
+    //   )
+    //   .then((res) => {
+    //     console.log(res.data);
+    //   });
+  };
+  return (
+    <div>
+      <Button type="primary" onClick={ajax}>
+        Button
+      </Button>
+    </div>
+  );
 }
